aed7dc206964ce4a1d39dde6151c8b90da3e8fd7
Sarp <94190460+sarp6@users.noreply.github.com>
2024-07-30 10:54:35 +0300

    Update CorrectUsage.cs

--- ocp/CorrectUsage.cs
+++ ocp/CorrectUsage.cs
@@ -1,1 +1,32 @@
 
+// OCP - Open/Closed Principle
+// Sınıflar genişletilmeye açık, değiştirilmeye kapalı olmalı.
+// Doğru Kullanım
+
+public interface IDiscount
+{
+    double ApplyDiscount(double price);
+}
+
+public class NoDiscount : IDiscount
+{
+    public double ApplyDiscount(double price)
+    {
+        return price;
+    }
+}
+
+public class PercentageDiscount : IDiscount
+{
+    private double percentage;
+
+    public PercentageDiscount(double percentage)
+    {
+        this.percentage = percentage;
+    }
+
+    public double ApplyDiscount(double price)
+    {
+        return price - (price * percentage / 100);
+    }
+}
