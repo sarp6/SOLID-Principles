3cb36497064171e5908e72b46a80cdd78bbc4eac
Sarp <94190460+sarp6@users.noreply.github.com>
2024-07-30 10:49:59 +0300

    Update CorrectUsage.cs

--- lsp/CorrectUsage.cs
+++ lsp/CorrectUsage.cs
@@ -1,1 +1,28 @@
 
+// LSP - Liskov Substitution Principle
+// Türeyen sınıflar, türediği sınıfların yerine kullanılabilmeli.
+// Doğru Kullanım
+
+public class Rectangle
+{
+    public virtual double Width { get; set; }
+    public virtual double Height { get; set; }
+
+    public double GetArea()
+    {
+        return Width * Height;
+    }
+}
+
+public class Square : Rectangle
+{
+    public override double Width
+    {
+        set { base.Width = base.Height = value; }
+    }
+
+    public override double Height
+    {
+        set { base.Width = base.Height = value; }
+    }
+}
