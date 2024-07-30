11cb484085d0498344d277a0770882ac469aac18
Sarp <94190460+sarp6@users.noreply.github.com>
2024-07-30 10:49:18 +0300

    Update CorrectUsage.cs

--- isp/CorrectUsage.cs
+++ isp/CorrectUsage.cs
@@ -1,1 +1,30 @@
+// ISP - Interface Segregation Principle
+// Kullanıcıları gereksiz arayüz metodları implement etmeye zorlamayın.
+// Doğru Kullanım
+
+public interface IPrinter
+{
+    void Print(string content);
+}
+
+public interface IScanner
+{
+    void Scan(string content);
+}
+
+public class Printer : IPrinter
+{
+    public void Print(string content)
+    {
+        Console.WriteLine("Printing: " + content);
+    }
+}
+
+public class Scanner : IScanner
+{
+    public void Scan(string content)
+    {
+        Console.WriteLine("Scanning: " + content);
+    }
+}
 
