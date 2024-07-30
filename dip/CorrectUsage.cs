ffa572ea929566d3311f401afd4f3155d9edfac4
Sarp <94190460+sarp6@users.noreply.github.com>
2024-07-30 10:46:18 +0300

    Update CorrectUsage.cs

--- dip/CorrectUsage.cs
+++ dip/CorrectUsage.cs
@@ -1,1 +1,31 @@
+// DIP - Dependency Inversion Principle
+// Yüksek seviye modüller düşük seviye modüllere bağımlı olmamalı. Her ikisi de soyutlamalara bağımlı olmalı.
+// Doğru Kullanım
 
+public interface IDiscount
+{
+    double ApplyDiscount(double price);
+}
+
+public class NoDiscount : IDiscount
+{
+    public double ApplyDiscount(double price)
+    {
+        return price;
+    }
+}
+
+public class OrderService
+{
+    private readonly IDiscount discount;
+
+    public OrderService(IDiscount discount)
+    {
+        this.discount = discount;
+    }
+
+    public double GetFinalPrice(double price)
+    {
+        return discount.ApplyDiscount(price);
+    }
+}
